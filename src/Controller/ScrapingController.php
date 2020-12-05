59723dbfa64fb0381f624aff121e23fc2176814e
achraf <achrafsmida@hotmail.fr>
2020-12-05 02:11:57 +0100

    init

--- src/Controller/ScrapingController.php
+++ src/Controller/ScrapingController.php
@@ -27,6 +27,31 @@ class ScrapingController extends AbstractController
         return $response->getContent();
     }
 
+    public  function getHtmlCurl($url){
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache",
+                "postman-token: 65d515b9-0f70-035a-1517-c3129966be83"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+        return $response ;
+
+    }
     /**
      * @Route("/", name="scraping")
      */
@@ -53,7 +78,7 @@ class ScrapingController extends AbstractController
             $datas = [];
             $url = $scraping['url'];
             try {
-                $html = $this->getHtmlFromUrl($url);
+                $html = $this->getHtmlCurl($url);
                 $crawler = new Crawler($html);
                 try {
                     $pages = $this->countPaginationPages($crawler->filter('span.pagination-compteur ')->text());
@@ -70,7 +95,7 @@ class ScrapingController extends AbstractController
                     $url = $scraping['url'];
                     if ($j > 1) $url .= "&page=" . $j;
 
-                    $html = $this->getHtmlFromUrl($url);
+                    $html = $this->getHtmlCurl($url);
 
                     $crawler = new Crawler($html);
 
